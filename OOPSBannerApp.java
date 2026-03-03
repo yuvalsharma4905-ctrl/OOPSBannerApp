93d59b3e511c5568999e3565c44824e033765758
yuvalsharma4905-ctrl <yuvalsharma4905@gmail.com>
2026-03-03 13:22:02 +0530

    feat:add uc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,15 @@
-public class OOPSBaneerApp {
+/*public class OOPSBaneerApp {
     public static void main(String[] args) {
 		system.out.println("oops"):
-	}
+	}*/
+}public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ****    ****    ****    **** ");
+        System.out.println("*    *  *    *  *    *  *     ");
+        System.out.println("*    *  *    *   ****    **** ");
+        System.out.println("*    *  *    *  *           *");
+        System.out.println(" ****    ****   *        **** ");
+    }
 }	
